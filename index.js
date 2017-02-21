d854e78c115a854173e676dc901c4548b6c1da10
Kieran Hall <kieran.hall@mindcurv.com>
2017-02-21 12:40:56 +0100

    Added all dependent libraries to the app.import method

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ module.exports = {
     included: function (app) {
         this._super.included(app);
         app.import(app.bowerDirectory + '/js-xlsx/dist/xlsx.js');
+        app.import(app.bowerDirectory + '/js-xlsx/dist/jszip.js');
+        app.import(app.bowerDirectory + '/js-xlsx/dist/cpexcel.js');
+        app.import(app.bowerDirectory + '/js-xlsx/dist/ods.js');
         app.import('vendor/xlsx.js', {
             exports: {
                 XLSX: ['default']
